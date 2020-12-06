69d9c48cfcefe5f010beb28e1f3436ebfc77a972
BuildTools <unconfigured@null.spigotmc.org>
2020-12-06 15:26:10 +0100

    finalisation class Vec2

--- NeuralSimulation/Math/Vec2.py
+++ NeuralSimulation/Math/Vec2.py
@@ -1,3 +1,5 @@
+import math
+
 class Vec2:
 
     def __init__(self, x=0, y=0, z=0):
@@ -22,3 +24,86 @@ class Vec2:
 
     def set_z(self, z):
         self.z = z
+
+    def add_x(self, a):
+        self.x += a
+
+    def add_y(self, a):
+        self.y += a
+
+    def add_z(self, a):
+        self.z += a
+
+    def sub_x(self, a):
+        self.x -= a
+
+    def sub_y(self, a):
+        self.y -= a
+
+    def sub_z(self, a):
+        self.z -= a
+
+    def mul_x(self, a):
+        self.x *= a
+
+    def mul_y(self, a):
+        self.y *= a
+
+    def mul_z(self, a):
+        self.z *= a
+
+    def div_x(self, a):
+        self.x /= a
+
+    def div_y(self, a):
+        self.y /= a
+
+    def div_z(self, a):
+        self.z /= a
+
+    def add(self, vec):
+        self.x += vec.x
+        self.y += vec.y
+        self.z += vec.z
+
+    def sub(self, vec):
+        self.x -= vec.x
+        self.y -= vec.y
+        self.z -= vec.z
+
+    def mul(self, vec):
+        self.x *= vec.x
+        self.y *= vec.y
+        self.z *= vec.z
+
+    def div(self, vec):
+        self.x /= vec.x
+        self.y /= vec.y
+        self.z /= vec.z
+
+    def magnitude(self):
+        return math.sqrt(self.x * self.x + self.y * self.y)
+
+    def normalize(self):
+        mag = self.magnitude()
+        self.x /= mag
+        self.y /= mag
+
+    def max(self):
+        return max(self.x, self.y)
+
+    def min(self):
+        return min(self.x, self.y)
+
+    def negate(self):
+        self.x = -self.x
+        self.y = -self.y
+
+    def copy(self):
+        return self
+
+    def to_string(self):
+        return str(self.x) + " " + str(self.y)
+
+    def equals(self, vec):
+        return self.x == vec.x and self.y == vec.y
