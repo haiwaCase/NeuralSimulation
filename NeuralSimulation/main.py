287844cae8e5550f28abb5ce5eb2c283ff57aec0
BuildTools <unconfigured@null.spigotmc.org>
2020-12-06 14:50:21 +0100

    pygame window

--- NeuralSimulation/main.py
+++ NeuralSimulation/main.py
@@ -1,1 +1,15 @@
-print("Hello World !")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("NeuralSimulation")
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
