7358a6ff1bda1b8a594b76d2d523165f8ae68e17
BuildTools <unconfigured@null.spigotmc.org>
2020-12-06 15:06:27 +0100

    class Entity et class Vec2

---
+++ NeuralSimulation/Entities/Entity.py
@@ -0,0 +1,4 @@
+class Entity:
+
+    def __init__(self, position):
+        self.position = position
---
+++ NeuralSimulation/Math/Vec2.py
@@ -0,0 +1,24 @@
+class Vec2:
+
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def get_x(self):
+        return self.x
+
+    def get_y(self):
+        return self.y
+
+    def get_z(self):
+        return self.z
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def set_z(self, z):
+        self.z = z
