3287c4b19ea0af05277036bc94e589826debc328
BuildTools <unconfigured@null.spigotmc.org>
2020-12-06 17:49:19 +0100

    Mise en place d'un système de scale

--- NeuralSimulation/Entities/Fox.py
+++ NeuralSimulation/Entities/Fox.py
@@ -1,9 +1,13 @@
 from Entities.Entity import Entity
 import pygame
 
+def load_image_fox(scale):
+    image = pygame.image.load("Res/fox.png")
+    return pygame.transform.rotozoom(image, 0, scale)
+
 class Fox(Entity, pygame.sprite.Sprite):
 
     def __init__(self, position):
         super().__init__(position, 20)
-        self.image = pygame.image.load("Res/fox.png")
+        self.image = load_image_fox(scale=12)
         self.rect = self.image.get_rect()
--- NeuralSimulation/Render/Renderer.py
+++ NeuralSimulation/Render/Renderer.py
@@ -1,7 +1,7 @@
 def update(pygame, entities):
     for entity in entities:
-        entity.set_position_x(entity.get_position().get_x() + 0.1)
-        entity.set_position_y(entity.get_position().get_y() + 0.1)
+        entity.set_position_x(entity.get_position().get_x() + 2)
+        entity.set_position_y(entity.get_position().get_y() + 2)
 
 def render(pygame, screen, entities):
     screen.fill((0, 0, 0))
