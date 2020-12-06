97509d9eb23981f3aa15bae00056621287c48672
BuildTools <unconfigured@null.spigotmc.org>
2020-12-06 16:28:27 +0100

    Ajout d'un système de traduction

--- NeuralSimulation/main.py
+++ NeuralSimulation/main.py
@@ -1,9 +1,21 @@
 import pygame
 import time
+import json
 import Render.Renderer as Renderer
 
 WIDTH = 1280
 HEIGHT = 720
+
+language = "fr_FR"
+
+def load_title():
+    with open(f"Lang/title_{language}.json") as json_data:
+        data_dict = json.load(json_data)
+
+    data_str = json.dumps(data_dict)
+
+    loaded_json = json.loads(data_str)
+    return loaded_json["Title"]
 
 def update():
     Renderer.update(pygame)
@@ -12,10 +24,11 @@ def render():
     Renderer.render(pygame)
 
 def main():
+    load_title()
     pygame.init()
 
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("NeuralSimulation")
+    pygame.display.set_caption("Neural Simulation")
 
     running = True
 
